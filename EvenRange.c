c25d9568e7fabd1ef46032ba158ec30271b318b1
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-06-30 07:33:12 +0000

    Update EvenRange.c

--- EvenRange.c
+++ EvenRange.c
@@ -1,9 +1,9 @@
-   #include<stdio.h>  
+    #include<stdio.h>  
     int main()
     {    
         int n1,n2,i;    
         scanf("%d%d",&n1,&n2);  
-        for(i=n1+1;i<=n2;i++)    
+        for(i=n1+1;i<n2;i++)    
         {    
             if(i%2==0)       
             {    
