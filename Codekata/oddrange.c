5ba340c4c45613f6e391103ee7ad7d80b3d9b8ff
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-06-30 07:19:12 +0000

    Update oddrange.c

--- Codekata/oddrange.c
+++ Codekata/oddrange.c
@@ -4,11 +4,13 @@
         int n1,n2,q=100000,i;    
         scanf("%d%d",&n1,&n2);  
         if(n2<q)
-        for(i=n1+1;i<=n2;i++)    
-        {    
-            if(i%2!=0)    
+        {
+            for(i=n1+1;i<=n2;i++)    
             {    
-                printf("%d\t",i);    
-            }    
-        }    
+                if(i%2!=0)    
+                {    
+                    printf("%d\t",i);    
+                }    
+            }
+        }
  }    
