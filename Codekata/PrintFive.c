166cba7fc06d464069d2fe356f9d6517bd5e6442
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-07-03 11:08:54 +0000

    Update PrintFive.c

--- Codekata/PrintFive.c
+++ Codekata/PrintFive.c
@@ -3,9 +3,11 @@ int main()
 {
     int sum=0,i,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<=5;i++)
     {
-        printf("%d\t",5*i);
+        printf("%d\t",n*i);
     }
     return 0;
 }
+
+
