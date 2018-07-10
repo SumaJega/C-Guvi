d64bdfd79d1b96a835fe50991a299fbb086303c8
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-07-10 09:32:13 +0000

    Create max.c

---
+++ Codekata/max.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,j,temp,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+temp=a[0];
+for(i=0;i<n;i++)
+{
+if(temp<a[i])
+{
+temp=a[i];
+}
+
+}
+
+printf("%d",temp);
+}
