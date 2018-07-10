37390bd3eb16b054f3c2d94fbaf618456c03d0c5
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-07-10 09:57:04 +0000

    Update sort.c

--- Codekata/sort.c
+++ Codekata/sort.c
@@ -1,26 +1,25 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-int a[10],i,j,n,temp;
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-for(i=0;i<n;i++)
-{
-for(j=1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}}
-}
-for(i=0;i<n;i++)
-{
-printf("%d\n",a[i]);
-}
-return 0;
+           int a[100],n,i,j,temp;
+           scanf("%d",&n);
+           for(i=0;i<n;i++)
+           scanf("%d",&a[i]);
+           for(i=0;i<n;i++)
+           {
+           	for(j=i+1;j<n;j++)
+           	{
+           		if(a[j]<a[i])
+           		{
+           	               temp=a[j];
+           	               a[j]=a[i];
+           	               a[i]=temp;
+           		}
+           	}
+           
+           }
+           for(i=0;i<n;i++)
+           printf("%d ",a[i]);
+           
+	return 0;
 }
