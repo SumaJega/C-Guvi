3aec9a6e125d55a717e00dd1685f0abea66b6b13
Jegadeeswari Ganapathy <38652953+SumaJega@users.noreply.github.com>
2018-09-11 11:08:48 +0000

    Update primeorder.c

--- Codekata/primeorder.c
+++ Codekata/primeorder.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-# include <math.h>
+#include <math.h>
 void primeFactors(int n)
 {
     int i;
@@ -9,7 +9,6 @@ printf("%d ",2);
 n = n/2;
 }
 
-
 for (i = 3; i <= sqrt(n); i = i+2)
 {
 while (n%i == 0)
@@ -17,6 +16,10 @@ while (n%i == 0)
 printf("%d ", i);
 n = n/i;
 }
+}
+if (n > 2)
+{
+printf ("%d ", n);
 }
 }
 int main()
